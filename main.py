a58faa10ecf2a6c8ec8a4404d4af882acb4b9b67
Ozan <47722606+oynozan@users.noreply.github.com>
2022-05-30 01:23:46 +0300

    Added algorithm selection

--- main.py
+++ main.py
@@ -7,6 +7,21 @@ algorithms = algorithms.Algorithms()
 #Screen
 WINDOW_WIDTH = 1000; WINDOW_HEIGHT = 600
 
+#Select Algorithm
+print(
+"""
+Algorithms:
+	1) Bubble Sort
+	2) Bogo Sort
+	3) Insertion Sort
+""")
+
+try:
+	algoSelection = int(input("Select Algorithm (number): "))
+except:
+	print("Only number inputs are allowed")
+	exit()
+
 pygame.init()
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -39,7 +54,15 @@ while running:
 
 		pygame.draw.rect(screen, (255,255,255), (rectX, rectY, rectWidth, rectHeight))
 
-	datas = algorithms.InsertionSort(datas)
+	if algoSelection == 1:
+		datas = algorithms.BubbleSort(datas)
+	elif algoSelection == 2:
+		datas = algorithms.BogoSort(datas)
+	elif algoSelection == 3:
+		datas = algorithms.InsertionSort(datas)
+	else:
+		print("Algorithm not found")
+		exit()
 	
-	clock.tick(120)
+	clock.tick(60) #FPS
 	pygame.display.update()
